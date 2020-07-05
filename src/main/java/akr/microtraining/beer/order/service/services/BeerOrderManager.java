12174a2b753ee2525711e384c960cdac114ffa0b
arodrigues_freebalance <arodrigues@freebalance.com>
2020-07-05 20:30:03 +0100

    Add Initial Beer Order Manager Closes #16

---
+++ src/main/java/akr/microtraining/beer/order/service/services/BeerOrderManager.java
@@ -0,0 +1,8 @@
+package akr.microtraining.beer.order.service.services;
+
+import akr.microtraining.beer.order.service.domain.BeerOrder;
+
+public interface BeerOrderManager {
+
+		BeerOrder newBewOrder(BeerOrder beerOrder );
+}
